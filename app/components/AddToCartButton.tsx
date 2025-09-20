2f23405df9be39b2289148ece8eb96e34e8beee6
nnamdi <uzonnamdi31@gmail.com>
2025-09-20 01:24:33 +0100

    new

--- app/components/AddToCartButton.tsx
+++ app/components/AddToCartButton.tsx
@@ -16,7 +16,7 @@ export default function AddToCartButton({ product }: AddToCartButtonProps) {
   return (
     <div
       onClick={() => addToCart(product)}
-      className="bg-[#7c6a46] hover:bg-[#56482d] cursor-pointer flex justify-center text-white py-2 px-6 rounded-[8px]"
+      className="bg-[#7c6a46] transition-transform duration-150 active:opacity-70 hover:bg-[#56482d] cursor-pointer flex justify-center text-white py-2 px-6 rounded-[8px]"
     >
       Add to Cart
     </div>
